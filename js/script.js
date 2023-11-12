87b2c16d5f05321e317d54216c564282f5d52fcb
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 08:37:00 -0600

    Update script.js to add icons to markers

--- js/script.js
+++ js/script.js
@@ -14,6 +14,15 @@ function initMap() {
     position: myLocation,
     map,
     title: "I used to live here!",
+    icon: "images/jersey.png"
+  });
+
+  var myLocation2 = new google.maps.LatLng(36.172, 115.139);
+  new google.maps.Marker({
+    position: myLocation2,
+    map,
+    title: "One of the best places I've been to!",
+    icon: "images/vegas.png"
   });
 }
 
