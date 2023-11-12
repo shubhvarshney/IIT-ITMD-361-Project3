d7516020cd3bb8f8a14bce05e9b3f752859f3130
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-11 22:40:18 -0600

    Update script.js to add details to map

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementbyId('map');
+  map = new google.maps.Map(document.getElementbyId('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  };
 }
