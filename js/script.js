16df02c53bfb31ae7af9d8b7f36cfa015ca2ac77
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 08:46:10 -0600

    Update script.js to add bounce animation

--- js/script.js
+++ js/script.js
@@ -10,20 +10,40 @@ function initMap() {
   });
 
   var myLocation = new google.maps.LatLng(40.518, 74.412);
-  new google.maps.Marker({
+  var jerseyMarker = new google.maps.Marker({
     position: myLocation,
     map,
     title: "I used to live here!",
+    animation: google.maps.Animation.DROP,
     icon: "images/jersey.png"
   });
+  jerseyMarker.addListener("click", toggleBounce1);
 
   var myLocation2 = new google.maps.LatLng(36.172, 115.139);
-  new google.maps.Marker({
+  var vegasMarker = new google.maps.Marker({
     position: myLocation2,
     map,
     title: "One of the best places I've been to!",
+    animation: google.maps.Animation.DROP,
     icon: "images/vegas.png"
   });
+  vegasMarker.addListener("click", toggleBounce2);
+}
+
+function toggleBounce1() {
+  if (jerseyMarker.getAnimation() !== null) {
+    jerseyMarker.setAnimation(null);
+  } else {
+    jerseyMarker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function toggleBounce2() {
+  if (vegasMarker.getAnimation() !== null) {
+    vegasMarker.setAnimation(null);
+  } else {
+    vegasMarker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 
 window.initMap = initMap;
