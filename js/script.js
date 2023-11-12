373d1f10139d7918b790479b0dc01ecf41b1137c
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 08:26:14 -0600

    Update script.js to add new marker

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@ var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementbyId('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 42.283, lng: 87.953},
+    zoom: 8,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ["roadmap", "terrain"],
+    }
+  });
+
+  var myLocation = new google.maps.LatLng(40.518, 74.412);
+  new google.maps.Marker({
+    position: myLocation,
+    map,
+    title: "I used to live here!",
   });
 }
 
