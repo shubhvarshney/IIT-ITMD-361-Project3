5b826bf591952fe29b31a160562dbf2b86da11ca
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 12:27:32 -0600

    Update script.js to add some more values to heatmap

--- js/script.js
+++ js/script.js
@@ -19,8 +19,26 @@ function initMap() {
     new google.maps.LatLng(42.280, -87.951),
     new google.maps.LatLng(42.280, -87.955),
     new google.maps.LatLng(42.280, -87.957),
-    new google.maps.LatLng(42.280, -87.969),
-    new google.maps.LatLng(42.280, -87.972)
+    new google.maps.LatLng(42.280, -88.969),
+    new google.maps.LatLng(42.280, -88.982),
+    new google.maps.LatLng(42.283, -88.983),
+    new google.maps.LatLng(42.283, -88.985),
+    new google.maps.LatLng(42.283, -88.947),
+    new google.maps.LatLng(42.283, -88.949),
+    new google.maps.LatLng(42.283, -88.951),
+    new google.maps.LatLng(42.283, -88.955),
+    new google.maps.LatLng(42.283, -88.957),
+    new google.maps.LatLng(42.283, -86.969),
+    new google.maps.LatLng(42.283, -86.972),
+    new google.maps.LatLng(42.280, -86.943),
+    new google.maps.LatLng(42.280, -86.945),
+    new google.maps.LatLng(42.280, -86.947),
+    new google.maps.LatLng(42.280, -86.949),
+    new google.maps.LatLng(42.280, -86.951),
+    new google.maps.LatLng(42.280, -86.955),
+    new google.maps.LatLng(42.280, -86.957),
+    new google.maps.LatLng(42.280, -86.969),
+    new google.maps.LatLng(42.280, -86.972),
   ];
   
   var map = new google.maps.Map(document.getElementById('map'), {
