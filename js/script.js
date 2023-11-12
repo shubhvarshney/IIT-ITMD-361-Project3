690c4561ec0f1a62047cce7c937f43d7fb8f6bc9
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 11:54:47 -0600

    Update script.js to change zoom and maptype controls

--- js/script.js
+++ js/script.js
@@ -22,10 +22,10 @@ function initMap() {
   
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.283, lng: -87.953},
-    zoom: 8,
+    zoom: 4,
     mapTypeControl: true,
     mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
       mapTypeIds: ["roadmap", "terrain"],
     }
   });
