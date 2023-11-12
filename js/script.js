65d8e23696954db08e88c12d6b05e89e0ea0aed8
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2023-11-12 12:22:39 -0600

    Update script.js to switch order of position attribute

--- js/script.js
+++ js/script.js
@@ -32,23 +32,23 @@ function initMap() {
 
   var myLocation = new google.maps.LatLng(40.518, -74.412);
   var jerseyMarker = new google.maps.Marker({
-    position: myLocation,
     map,
     title: "I used to live here!",
     draggable: true,
     animation: google.maps.Animation.DROP,
     icon: "images/jersey.png",
+    position: myLocation,
   });
   jerseyMarker.addListener("click", toggleBounce1);
 
   var myLocation2 = new google.maps.LatLng(36.172, -115.139);
   var vegasMarker = new google.maps.Marker({
-    position: myLocation2,
     map,
     title: "One of the best places I've been to!",
     draggable: true,
     animation: google.maps.Animation.DROP,
     icon: "images/vegas.png",
+    position: myLocation2,
   });
   vegasMarker.addListener("click", toggleBounce2);
 
